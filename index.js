c47e34bd01404fc4a95b0fa398754e0ffbbf2002
Joe Huelsman <kcgooner91@gmail.com>
2020-02-17 18:54:49 -0600

    added css and html to all files so they are now uniform in design

--- index.js
+++ index.js
@@ -4,26 +4,3 @@ const manager = ("./templates/manager.js");
 const engineer = ("./templates/engineer.js");
 const intern = ("./templates/intern.js");
 
-
-inquirer.prompt([{
-    type: "confirm",
-    message: "Add a new team member?",
-    name: "users",
-    prompts: [
-        {
-            type: "list",
-            message: "Choose team member's role:",
-            name: "role",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern",
-            ],
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "What is member's Name? ",
-        }
-    ]
-}])
