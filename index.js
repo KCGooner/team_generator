9efa51e333dd53f6876ee266a7e85bd9faf90010
Joe Huelsman <kcgooner91@gmail.com>
2020-02-17 18:01:35 -0600

    added html to files and began js files

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const manager = ("./templates/manager.js");
+const engineer = ("./templates/engineer.js");
+const intern = ("./templates/intern.js");
+
+
