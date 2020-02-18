816196424038935cd12c9cc728b1804824786057
Joe Huelsman <kcgooner91@gmail.com>
2020-02-17 18:28:01 -0600

    added html to main file and added some css for employees

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ const engineer = ("./templates/engineer.js");
 const intern = ("./templates/intern.js");
 
 
+inquirer.prompt([{
+    type: "confirm",
+    message: "Add a new team member?",
+    name: "users",
+    prompts: [
+        {
+            type: "list",
+            message: "Choose team member's role:",
+            name: "role",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+            ],
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is member's Name? ",
+        }
+    ]
+}])
